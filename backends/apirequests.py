dd9a3291160ad0e728d0546db9dd216e4900e94f
Matthew Phan <phannm@uci.edu>
2023-02-04 12:33:25 -0800

    API Request Demo

--- backends/apirequests.py
+++ backends/apirequests.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import requests
+
+courses = requests.get("https://api.peterportal.org/rest/v0/courses/all")
+courses_data = courses.json()
+
+for course in courses_data:
+    course_dict = dict(course)
+    id_name = course_dict["id"]
+    department = course_dict["department"]
+    print(id_name, department)
