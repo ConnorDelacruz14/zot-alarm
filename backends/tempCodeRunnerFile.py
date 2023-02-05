9b6811de60a9349697f55d7942f1596a0d68bf79
Matthew Phan <phannm@uci.edu>
2023-02-04 19:50:41 -0800

    same thing as last

--- backends/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-if not check_time_conflict(user_courses):
-            print("The following course ", user_dep, user_cdnum, " has not been added due to schedule conflicts.", sep = "")
-            user_courses.pop()
