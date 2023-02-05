f299ded77c7c959639c44d0f448de7dfae4276e1
Matthew Phan <phannm@uci.edu>
2023-02-04 16:11:50 -0800

    successful connection to petrportal

--- backends/apirequests.py
+++ backends/apirequests.py
@@ -1,10 +1,53 @@
 import requests
 
-courses = requests.get("https://api.peterportal.org/rest/v0/courses/all")
-courses_data = courses.json()
+def get_all_courses():
+    courses = requests.get("https://api.peterportal.org/rest/v0/courses/all") #connects to PetrPortalAPI
+    courses_data = courses.json()
+    all_departments_info = {}
+    for course in courses_data:
+        course_dict = dict(course)
+        number = course_dict["number"]
+        department_name = course_dict["department"]
+        terms = course_dict["terms"]
+        if department_name in all_departments_info.keys():
+            all_departments_info[department_name].append(number)
+        else:
+            all_departments_info[department_name] = [number]
+    return all_departments_info
 
-for course in courses_data:
-    course_dict = dict(course)
-    id_name = course_dict["id"]
-    department = course_dict["department"]
-    print(id_name, department)
+def get_sections(term, quarter, department, number):
+    if type(int(term)) is int:
+        quarter = quarter.upper()
+        department = department.upper()
+        if department == "CRM/LAW":
+            department = "CRM%2FLAW"
+        elif department == "I&C SCI":
+            department = "I%26C SCI"
+        schedule = requests.get(f"https://api.peterportal.org/rest/v0/schedule/soc?term={term}%20{quarter}&department={department}&courseNumber={number}")
+        schedule_dict = dict(schedule.json())
+        # meetings = schedule_dict['schools'][0]['departments'][0]['courses'][0]['sections'][0]['meetings'][0]
+        # section_code = schedule_dict['schools'][0]['departments'][0]['courses'][0]['sections'][0]['sectionCode']
+        
+        return schedule_dict['schools'][0]['departments'][0]['courses'][0]['sections']
+    else:
+        raise Exception("No Specified Course Found")
+
+def get_meeting_info(sections_list, section_code):
+    for section in sections_list:
+        if section['sectionCode'] == str(section_code):
+            return (section['instructors'], section['meetings'], section['finalExam'])
+# for key, value in all_departments_info.items():
+#     print(key, value)
+
+#print(get_schedule(2023,"WINTER", "I&C SCI", "45C")['schools'][0]['departments'][0]['courses'][0]['sections'][0]['sectionCode'])
+
+# print(get_meeting_info(get_sections(2023, "Winter", "STATS", 67), 37170))
+
+# class_info = get_schedules(2023, "Winter", "STATS", "67")
+# print(get_meeting_info(get_sections(2023, "Winter", "STATS", 67), 37200))
+
+# schedule = requests.get(f"https://api.peterportal.org/rest/v0/schedule/soc?term=2023%23FALL&department=I%26C SCI&courseNumber=45C")
+# print(schedule.json())
+# sched_data = dict(schedule.json())
+
+# print(sched_data)
