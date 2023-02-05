82ede6c8a441577db7c143283736d9584333be61
Matthew Phan <phannm@uci.edu>
2023-02-05 08:28:50 -0800

    ai fixed

---
+++ backends/ai/execute.py
@@ -0,0 +1,5 @@
+from wise_words import *
+
+if __name__ == "__main__":
+    print(generateWiseWords())
+    print(generateMotivationalImageLink())
--- backends/ai/wise_words.py
+++ backends/ai/wise_words.py
@@ -1,24 +1,23 @@
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
-prompt = input("Human: ")
 
-# response = openai.Completion.create(
-#     model="text-davinci-001",
-#     prompt=prompt,
-#     temperature=0.6
-# )
+def generateWiseWords():
+  words = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="I miss my lecture today, give me quote to motivate me not to be late again.",
+    temperature=1,
+    max_tokens = 200
+  )
+  return dict(words)["choices"][0]["text"].replace("\n", "")
 
-# print("Wise words:", )
-response = openai.Image.create(
-  prompt=prompt,
-  n=1,
-  size="512x512"
-)
 
-image_url = response['data'][0]['url']
-print(image_url)
-
-print("AI:", dict(response)["choices"][0]["text"].replace("\n", ""))
+def generateMotivationalImageLink():
+  image = openai.Image.create(
+    prompt="person encouraging face",
+    size="1024x1024",
+  )
+  image_url = image['data'][0]['url']
+  return image_url
