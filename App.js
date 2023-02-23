c32e6781eba9dd7d824bd762b4bf945ff3b6ef90
Connor Delacruz <63576585+ConnorDelacruz14@users.noreply.github.com>
2023-02-23 11:40:50 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Homepage from "./pages/Homepage";
+import Loginpage from "./pages/Loginpage";
+import CourseAdd from "./pages/CourseAdd";
+import Alarms from "./pages/Alarms";
+import Friends from "./pages/Friends";
+import Graphs from "./pages/Graphs";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Zot Alarm!">
+        <Stack.Screen name="Zot Alarm!" component={Loginpage} />
+        <Stack.Screen
+          name="Homepage"
+          component={Homepage}
+          options={() => ({
+            headerBackVisible: false,
+          })}
+        />
+        <Stack.Screen
+          name="CourseAdd"
+          component={CourseAdd}
+          options={() => ({
+            headerBackVisible: false,
+          })}
+        />
+        <Stack.Screen
+          name="Alarms"
+          component={Alarms}
+          options={() => ({
+            headerStyle: {
+              backgroundColor: "black",
+            },
+          })}
+        ></Stack.Screen>
+        <Stack.Screen name="Friends" component={Friends}></Stack.Screen>
+        <Stack.Screen name="Graphs" component={Graphs}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
